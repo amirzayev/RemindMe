ecdc3e9d9824050ce73bcb1f28f1cd51d863934a
amirzayev <amirzayev13@gmail.com>
2017-10-06 15:05:24 +0400

    implemented navigationView and tab navigation with test fragment

---
+++ app/src/main/java/com/amirzayev/remindme/Constants.java
@@ -0,0 +1,9 @@
+package com.amirzayev.remindme;
+
+
+public class Constants {
+
+    public static final int TAB_ONE = 0;
+    public static final int TAB_TWO = 1;
+    public static final int TAB_TREE = 2;
+}
--- app/src/main/java/com/amirzayev/remindme/MainActivity.java
+++ app/src/main/java/com/amirzayev/remindme/MainActivity.java
@@ -1,8 +1,11 @@
 package com.amirzayev.remindme;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -65,5 +68,30 @@ public class MainActivity extends AppCompatActivity {
     private void initNavigationView() {
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 
+        //создаем иконку для нажатия чтобы открыть (без свайпа)
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.view_navigation_open, R.string.view_navigation_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        //создаем navigationView
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                drawerLayout.closeDrawers();
+
+                switch (menuItem.getItemId()) {
+
+                    case R.id.actionNotificationItem:
+                        showNotificationTab();
+                }
+                return true;
+            }
+        });
+
+    }
+
+    private void showNotificationTab(){
+        viewPager.setCurrentItem(Constants.TAB_TWO);
     }
 }
