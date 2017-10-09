0db191f8a0eafa0d1b10e8cddf22b8584bd7e894
amirzayev <amirzayev13@gmail.com>
2017-10-09 17:38:37 +0400

    added CardView

---
+++ app/src/main/java/com/amirzayev/remindme/dto/RemindDTO.java
@@ -0,0 +1,19 @@
+package com.amirzayev.remindme.dto;
+
+
+public class RemindDTO {
+
+    public String title;
+
+    public RemindDTO(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
--- app/src/main/java/com/amirzayev/remindme/fragment/HistoryFragment.java
+++ app/src/main/java/com/amirzayev/remindme/fragment/HistoryFragment.java
@@ -4,15 +4,22 @@ package com.amirzayev.remindme.fragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.amirzayev.remindme.R;
+import com.amirzayev.remindme.adapter.RemindListAdapter;
+import com.amirzayev.remindme.dto.RemindDTO;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HistoryFragment extends AbstarctTabFragment {
 
-    private static final int LAYOUT = R.layout.fragment_example;
+    private static final int LAYOUT = R.layout.fragment_history;
 
 
     public static HistoryFragment getInstans(Context context) {
@@ -30,7 +37,25 @@ public class HistoryFragment extends AbstarctTabFragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         view = inflater.inflate(LAYOUT, container, false);
+
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+        recyclerView.setAdapter(new RemindListAdapter(createMockRemindListData()));
+
         return view;
+    }
+
+    private List<RemindDTO> createMockRemindListData() {
+
+        List<RemindDTO> data = new ArrayList<>();
+        data.add(new RemindDTO("Item 1"));
+        data.add(new RemindDTO("Item 2"));
+        data.add(new RemindDTO("Item 3"));
+        data.add(new RemindDTO("Item 4"));
+        data.add(new RemindDTO("Item 5"));
+        data.add(new RemindDTO("Item 6"));
+
+        return data;
     }
 
 
